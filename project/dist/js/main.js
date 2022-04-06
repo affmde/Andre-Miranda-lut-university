d3d2040de0fecf02436ddbeb79ef2e3024a03f8e
André Miranda <affmde@hotmail.com>
2022-04-06 10:49:22 +0300

    Homepage and Core Sass/CSS

---
+++ project/dist/js/main.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding')
+const navItem = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+const toggleMenu = () => {
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('close');
+        menuNav.classList.add('close');
+        menuBranding.classList.add('close');
+        navItem.forEach(item=> item.classList.add('show'))
+
+        showMenu=true
+    }else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('close');
+        menuNav.classList.remove('close');
+        menuBranding.classList.remove('close');
+        navItem.forEach(item=> item.classList.remove('show'))
+
+        showMenu=false
+    }
+}
+
+
+
+
